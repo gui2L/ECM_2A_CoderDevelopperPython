9739ff59a4fbb1210371fcbe7dd3baa6245c8dc4
gui2L <gsurleau@gmail.com>
2025-10-12 04:22:32 +0200

    add story chevauchement

---
+++ story_bateau.py
@@ -0,0 +1,20 @@
+from bateau import Bateau
+
+#   Nom : "chevauchement"
+#   Utilisateur : un joueur
+#   Story : Positionner des bateaux sans chevauchement
+
+#   Actions :
+
+#1.créer un bateau b1
+b1 = Bateau(2, 3, 4)
+
+
+#2.créer un bateau b2
+b2 = Bateau(1, 4, 2, True)
+
+#3.créer un bateau b3
+b3 = Bateau(1, 4)
+
+
+#3.Vérifier si les deux bateaux se chevauchent
