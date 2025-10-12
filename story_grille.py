6bbbb4d147f2ad8c48d96969334f74df52865bf5
gui2L <gsurleau@gmail.com>
2025-10-12 03:05:34 +0200

    update story_grille

--- story_grille.py
+++ story_grille.py
@@ -1,8 +1,15 @@
 from grille import Grille
 
+#   Nom : "Plouf dans l'eau"
+#   Utilisateur : un joueur
+#   Story : On veut pouvoir gérer les tirs de l'adversaire
+
+#   Actions :
+
 #1. créer une grille à 5 lignes et 8 colonnes
 g = Grille(5, 8)
 
+#(5.)
 while True:
     #2. afficher la grille à l'écran
     g.afficher()
@@ -13,7 +20,7 @@ while True:
             x, y = map(int, input("Entrez les coordonnées x et y séparées par un espace : ").split())
             break  
         except ValueError:
-            print("Entrée invalide ! Veuillez entrer deux nombres séparés par un espace.")
+            print("Saisie invalide ! Veuillez entrer deux nombres séparés par un espace.")
 
     #4. tirer à l'endroit indiqué sur la grille
     g.tirer(x, y)
