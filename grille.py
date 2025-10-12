49bfd45904231a3b03555b38c4006288aff4f0a6
gui2L <gsurleau@gmail.com>
2025-10-12 02:06:27 +0200

    add methodes d'affichage

--- grille.py
+++ grille.py
@@ -3,5 +3,28 @@ class Grille:
 
 
     def __init__(self, nombre_lignes=0, nombre_colonnes=0):
-        self.matrice = [Grille.vide*nombre_colonnes*nombre_lignes]
+        self.matrice = [Grille.vide for i in range(nombre_colonnes*nombre_lignes)]
         self.nombre_colonnes = nombre_colonnes
+        self.nombre_lignes = nombre_lignes
+
+    def __str__(self):
+        lignes = []
+        for i in range(self.nombre_lignes):
+            ligne = ""
+            for j in range(self.nombre_colonnes):
+                ligne += self.matrice[i*self.nombre_colonnes+j]
+            lignes.append(ligne)
+        return "\n".join(lignes)
+
+    def afficher(self):
+        for i in range(self.nombre_lignes):
+            ligne = ""
+            for j in range(self.nombre_colonnes):
+                ligne += self.matrice[i*self.nombre_colonnes+j]
+            print(ligne)
+
+
+    
+        
+        
+
