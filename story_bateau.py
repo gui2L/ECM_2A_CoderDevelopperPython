a37dc5a2b23789dc22ae278cd47b2b7f175e33d5
gui2L <gsurleau@gmail.com>
2025-10-18 22:05:40 +0200

    feat: adjust display

--- story_bateau.py
+++ story_bateau.py
@@ -15,6 +15,9 @@ b2 = Bateau(1, 4, 2, True)
 #3.créer un bateau b3
 b3 = Bateau(3, 5, 4)
 
+print(f"b1 : {b1.positions}")
+print(f"b2 : {b2.positions}")
+print(f"b3 : {b3.positions}\n")
 
 #3.Vérifier si les deux bateaux se chevauchent
 if b1.chevauche(b2):
@@ -22,8 +25,12 @@ if b1.chevauche(b2):
 else:
     print("b1 ne chevauche pas b2")
 
-
 if b1.chevauche(b3):
     print("b1 chevauche b3")
 else:
     print("b1 ne chevauche pas b3")
+
+if b2.chevauche(b3):
+    print("b2 chevauche b3")
+else:
+    print("b2 ne chevauche pas b3")
