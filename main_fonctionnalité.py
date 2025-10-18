9237c5e896ef6c0f19ee48864bf5eb4339659175
gui2L <gsurleau@gmail.com>
2025-10-18 23:09:17 +0200

    feat: add small tests for method ajoute class Grille

--- main_fonctionnalité.py
+++ main_fonctionnalité.py
@@ -1,4 +1,5 @@
 from grille import Grille
+from bateau import Bateau
 
 def main():
     
@@ -13,6 +14,18 @@ def main():
     g.tirer(4, 3)
     g.tirer(5, 8)
 
+    print(g)
+    print()
+
+    b1 = Bateau(2, 3, 4)
+    b2 = Bateau(1, 4, 2, True)
+    b3 = Bateau(3, 5, 4)
+    b4 = Bateau(3, 5, 9)
+    g.ajoute(b1)
+    g.ajoute(b2)
+    g.ajoute(b3)
+    g.ajoute(b4)
+    
     print(g)
     return 0
 
