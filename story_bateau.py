859a8d5533c8c7f879a89c74c6505e45f63a8894
gui2L <gsurleau@gmail.com>
2025-10-18 21:58:23 +0200

    feat: chevauchement verif

--- story_bateau.py
+++ story_bateau.py
@@ -9,12 +9,21 @@ from bateau import Bateau
 #1.créer un bateau b1
 b1 = Bateau(2, 3, 4)
 
-
 #2.créer un bateau b2
 b2 = Bateau(1, 4, 2, True)
 
 #3.créer un bateau b3
-b3 = Bateau(1, 4)
+b3 = Bateau(3, 5, 4)
 
 
 #3.Vérifier si les deux bateaux se chevauchent
+if b1.chevauche(b2):
+    print("b1 chevauche b2")
+else:
+    print("b1 ne chevauche pas b2")
+
+
+if b1.chevauche(b3):
+    print("b1 chevauche b3")
+else:
+    print("b1 ne chevauche pas b3")
